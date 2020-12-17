dbf7b1f9ec70e6fa8245046db9d7d122d6e89456
dangdung1996 <dung1996.it@gmail.com>
2020-12-17 14:50:06 +0700

    init

---
+++ src/MoveablePointTest.java
@@ -0,0 +1,9 @@
+public class MoveablePointTest {
+    public static void main(String[] args) {
+        MoveablePoint moveablePoint = new MoveablePoint();
+        System.out.println(moveablePoint);
+
+        moveablePoint = new MoveablePoint(5,8);
+        System.out.println(moveablePoint);
+    }
+}
